44893f4071be6d46a3a83dda29879869b06108fe
114OPk <607762@student.dusd.net>
2022-09-22 15:43:19 -0700

    Inside the room
    I have added something in my room.

---
+++ BenjiVRRoom/Assets/DateTime.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DateTime : MonoBehaviour
+{
+
+    public GameObject Clock_Analog_Red;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
