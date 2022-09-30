376e695abe62fee77a8c1426d023a4134c8b5024
114OPk <607762@student.dusd.net>
2022-09-30 15:25:36 -0700

    Clock
    Clock fixed

--- BenjiVRRoom/Assets/Clock.cs
+++ BenjiVRRoom/Assets/Clock.cs
@@ -24,13 +24,13 @@ public class Clock : MonoBehaviour
     {
         DateTime currentTime = DateTime.Now;
 
-        float secondsDegree = -(currentTime.Second / 60f) * 360f;
-        secondsHand.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, secondsDegree));
+        float secondsDegree = (currentTime.Second / 60f) * 360f;
+        secondsHand.transform.localRotation = Quaternion.Euler(new Vector3(secondsDegree, 0, 0));
 
-        float minutesDegree = -(currentTime.Minute / 60f) * 360f;
-        minutesHand.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, minutesDegree));
+        float minutesDegree = (currentTime.Minute / 60f) * 360f;
+        minutesHand.transform.localRotation = Quaternion.Euler(new Vector3(minutesDegree, 0, 0));
 
-        float hoursDegree = -(currentTime.Hour / 12f) * 360f;
-        hoursHand.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, hoursDegree));
+        float hoursDegree = (currentTime.Hour / 12f) * 360f;
+        hoursHand.transform.localRotation = Quaternion.Euler(new Vector3(hoursDegree, 0, 0));
     }
 }
